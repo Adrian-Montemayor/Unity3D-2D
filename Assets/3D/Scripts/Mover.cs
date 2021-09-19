3c197718308bef0fd4ca7097aca381c499102487
Adrian-Montemayor <adrianmoncar@hotmail.com>
2021-09-18 23:06:03 -0500

    Add: cinemamachine follow camera

--- Assets/3D/Scripts/Mover.cs
+++ Assets/3D/Scripts/Mover.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * _speed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * _speed;
+        transform.Translate(xValue, 0, zValue);
     }
 }
