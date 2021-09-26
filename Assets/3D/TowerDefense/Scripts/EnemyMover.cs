3f9b01d2ee4d82b22547239496ec3e75febfc0f7
Adrian-Montemayor <adrianmoncar@hotmail.com>
2021-09-25 19:28:18 -0500

    add: movement script for enemies and new assets

---
+++ Assets/3D/TowerDefense/Scripts/EnemyMover.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField]
+    List<WayPoint> path = new List<WayPoint>();
+    [SerializeField]
+    float waitTime = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintWayPointName();
+        StartCoroutine("MoveRoutine");
+    }
+
+    void PrintWayPointName()
+    {
+        foreach(var point in path)
+        {
+            Debug.Log(point.name);
+        }
+    }
+
+    IEnumerator MoveRoutine()
+    {
+        yield return new WaitForSeconds(2f);
+        foreach(var point in path)
+        {
+            transform.position = point.transform.position;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
